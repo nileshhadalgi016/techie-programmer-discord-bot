7e61073964c4e634085a9197e2f9790cbf998e82
sadiqshaik <54494934+iamsadiqshaik1@users.noreply.github.com>
2020-09-05 20:12:55 +0530

    Update techie_discord_bot.py

--- techie_discord_bot.py
+++ techie_discord_bot.py
@@ -1,11 +1,42 @@
 import discord
 from discord.ext import commands
+import pyjokes
+from quotes import Quotes
+import wikipedia
+from animals import Animals
+import emojis
+from covid import Covid
 import os
+
 def hellow():
     return "print('hello world')"
 def binodw():
     return ("binod\n")
+def img1():
+    an=Animals('dog')
+    return (an.image())
+def wiki1(query):
+    result=wikipedia.summary(query,sentences=2)
+    return result
+def jokes1():
+    k=emojis.encode(":smile:")
+    return (pyjokes.get_joke()+k*3)
+def quotes1():
+    quotes = Quotes()
+    persons = quotes.random()
+    l=str(persons[1])
+    k=emojis.encode(":sunny:")
+    return l+k*2
 
+
+def covid(arg):
+    case=str(arg)
+    #covid=Covid(source="worldometers")
+    covid=Covid()
+    cases=covid.get_status_by_country_name(str(case))
+    # for x in cases:
+    #     k=x,":",cases[x]
+    return(cases)
 client = commands.Bot(command_prefix=">")
 
 @client.event
@@ -59,4 +90,19 @@ sys.stdout = open('output.txt', 'wt')
             arg = arg + ''.join(op)
         await ctx.send(arg)
 
+@client.command()
+async def img(ctx):
+    await ctx.send(img1())
+@client.command()
+async def quotes(ctx):
+    await ctx.send(quotes1())
+@client.command()
+async def jokes(ctx):
+    await ctx.send(jokes1())
+@client.command()
+async def wiki(ctx,*,arg):
+    await ctx.send(wiki1(arg))
+@client.command()
+async def covid_news(ctx,*,arg):
+    await ctx.send(covid(arg))
 client.run('api key')
